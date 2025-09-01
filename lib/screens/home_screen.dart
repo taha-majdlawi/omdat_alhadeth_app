acd8a4035f900e7d1be416070e7db67b2d80cffa
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-09-01 12:39:47 +0300

    creating books card

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,7 +6,12 @@ import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
+  final List<String> books = const [
+    'كتاب الصلاة',
+    'كتاب الزكاة',
+    'كتاب الصيام',
+    'كتاب الحج',
+  ];
   @override
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context);
@@ -22,7 +27,58 @@ class HomeScreen extends StatelessWidget {
           style: TextStyle(fontFamily: 'Reem Kufi'),
         ),
       ),
-      body: Center(child: Text("")),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(height: 25),
+            Padding(
+              padding: const EdgeInsets.only(right: 9.0),
+              child: Text(
+                "تصنيفات الكتب",
+                textAlign: TextAlign.right,
+
+                style: TextStyle(
+                  fontSize: 18,
+                  fontFamily: "Reem Kufi",
+                  color: themeProvider.getIsDarkTheme
+                      ? const Color.fromARGB(255, 255, 255, 255)
+                      : const Color.fromARGB(255, 0, 0, 0),
+                ),
+              ),
+            ),
+
+            Expanded(
+              child: ListView.builder(
+                itemCount: books.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    borderOnForeground: true,
+                    color: themeProvider.getIsDarkTheme
+                        ? AppColors.lightCardColor
+                        : AppColors.darkScaffoldColor,
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Text(
+                        textAlign: TextAlign.right,
+                        books[index],
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontFamily: "Reem Kufi",
+                          color: themeProvider.getIsDarkTheme
+                              ? Colors.black
+                              : Colors.white,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
