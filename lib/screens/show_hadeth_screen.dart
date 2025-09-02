ad50aaf89b675e8e85488fbff6471868b86cc7b8
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-09-02 11:55:20 +0300

    create ShowHadethScreen

--- lib/screens/show_hadeth_screen.dart
+++ lib/screens/show_hadeth_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class ShowHadethScreen extends StatelessWidget {
   const ShowHadethScreen({super.key});
-
+ 
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
