d71ad4bf3ec077aa70e27bdc5ac8b0f96e93f91b
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-09-02 11:42:24 +0300

    refactor

--- lib/screens/hadeth_screen.dart
+++ lib/screens/hadeth_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:omdat_alhadeth/core/constants/app_colors.dart';
 import 'package:omdat_alhadeth/providers/theme_provider.dart';
+import 'package:omdat_alhadeth/widgets/hadethes_listView_widget.dart';
 
 class HadethsScreen extends StatelessWidget {
   const HadethsScreen({
@@ -72,55 +73,6 @@ class HadethsScreen extends StatelessWidget {
             HadethesListView(hadethText: hadethText, themeProvider: themeProvider),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class HadethesListView extends StatelessWidget {
-  const HadethesListView({
-    super.key,
-    required this.hadethText,
-    required this.themeProvider,
-  });
-
-  final List<String> hadethText;
-  final ThemeProvider themeProvider;
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: ListView.builder(
-        itemCount: hadethText.length,
-        itemBuilder: (context, index) {
-          return Card(
-            borderOnForeground: true,
-            color: themeProvider.getIsDarkTheme
-                ? AppColors.lightCardColor
-                : AppColors.darkScaffoldColor,
-            child: Padding(
-              padding: const EdgeInsets.only(
-                left: 15.0,
-                right: 15.0,
-                top: 15.0,
-                bottom: 15.0,
-              ),
-              child: Text(
-                textAlign: TextAlign.right,
-                hadethText[index],
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-                style: TextStyle(
-                  fontSize: 22,
-                  fontFamily: "Reem Kufi",
-                  color: themeProvider.getIsDarkTheme
-                      ? Colors.black
-                      : Colors.white,
-                ),
-              ),
-            ),
-          );
-        },
       ),
     );
   }
