eeb7b2eeb2058c74bbcddd0928dafc6aee4b0e17
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-09-02 10:37:37 +0300

    create HadethsScreen

---
+++ lib/screens/hadeth_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HadethsScreen extends StatelessWidget {
+  const HadethsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
