2bb0421ffbecb5f9b719a345fc648021ff8c68c4
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-09-02 11:52:24 +0300

    create HadethItemCardWidget

---
+++ lib/widgets/hadeth_item_card_widget.dart
@@ -0,0 +1,44 @@
+
+import 'package:flutter/material.dart';
+import 'package:omdat_alhadeth/core/constants/app_colors.dart';
+import 'package:omdat_alhadeth/providers/theme_provider.dart';
+
+class HadethItemCardWidget extends StatelessWidget {
+  const HadethItemCardWidget({
+    super.key,
+    required this.themeProvider,
+    required this.hadethText,
+  });
+
+  final ThemeProvider themeProvider;
+  final String hadethText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      borderOnForeground: true,
+      color: themeProvider.getIsDarkTheme
+          ? AppColors.lightCardColor
+          : AppColors.darkScaffoldColor,
+      child: Padding(
+        padding: const EdgeInsets.only(
+          left: 15.0,
+          right: 15.0,
+          top: 15.0,
+          bottom: 15.0,
+        ),
+        child: Text(
+          textAlign: TextAlign.right,
+          hadethText,
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+          style: TextStyle(
+            fontSize: 22,
+            fontFamily: "Reem Kufi",
+            color: themeProvider.getIsDarkTheme ? Colors.black : Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/hadethes_listView_widget.dart
+++ lib/widgets/hadethes_listView_widget.dart
@@ -1,7 +1,7 @@
-
 import 'package:flutter/material.dart';
 import 'package:omdat_alhadeth/core/constants/app_colors.dart';
 import 'package:omdat_alhadeth/providers/theme_provider.dart';
+import 'package:omdat_alhadeth/widgets/hadeth_item_card_widget.dart';
 
 class HadethesListView extends StatelessWidget {
   const HadethesListView({
@@ -19,35 +19,13 @@ class HadethesListView extends StatelessWidget {
       child: ListView.builder(
         itemCount: hadethText.length,
         itemBuilder: (context, index) {
-          return Card(
-            borderOnForeground: true,
-            color: themeProvider.getIsDarkTheme
-                ? AppColors.lightCardColor
-                : AppColors.darkScaffoldColor,
-            child: Padding(
-              padding: const EdgeInsets.only(
-                left: 15.0,
-                right: 15.0,
-                top: 15.0,
-                bottom: 15.0,
-              ),
-              child: Text(
-                textAlign: TextAlign.right,
-                hadethText[index],
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-                style: TextStyle(
-                  fontSize: 22,
-                  fontFamily: "Reem Kufi",
-                  color: themeProvider.getIsDarkTheme
-                      ? Colors.black
-                      : Colors.white,
-                ),
-              ),
-            ),
+          return HadethItemCardWidget(
+            themeProvider: themeProvider,
+            hadethText: hadethText[index],
           );
         },
       ),
     );
   }
 }
+
