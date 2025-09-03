8b50c96e081149e3b3a8b5f653660c3b970d83f2
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-09-03 18:40:38 +0300

    fixed an error and refactor hadeth text

--- lib/screens/show_hadeth_screen.dart
+++ lib/screens/show_hadeth_screen.dart
@@ -8,6 +8,8 @@ class ShowHadethScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: themeProvider.getIsDarkTheme
@@ -18,11 +20,130 @@ class ShowHadethScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Text('This is the Show Hadeth Screen'),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Text(
+                textAlign: TextAlign.right,
+                style: TextStyle(
+                  fontSize: 24,
+                  fontFamily: 'Amiri',
+                  color: themeProvider.getIsDarkTheme
+                      ? Colors.white
+                      : Colors.black,
+                ),
+                "\u202Bعن عمرَ بنِ الخطابِ رضي الله عنه قال: سمعتُ رسولَ الله ﷺ يقول: «إنما الأعمالُ بالنيَّاتِ – وفي روايةٍ: بالنيَّةِ – وإنما لكلِّ امرئٍ ما نوى. فمَن كانت هجرتُه إلى الله ورسولِه؛ فهجرتُه إلى الله ورسولِه. ومَن كانت هجرتُه إلى دنيا يُصيبُها أو امرأةٍ يتزوَّجُها؛ فهجرتُه إلى ما هاجرَ إليه».\u202C",
+              ),
+            ],
+          ),
         ),
+      ),
+      bottomSheet: HadethScreenBottomSheet(
+        screenWidth: screenWidth,
+        screenHeight: screenHeight,
+      ),
+
+      // bottomNavigationBar: BottomNavigationBar(
+      //   items: [
+      //     BottomNavigationBarItem(icon: Icon(Icons.abc), label: 'hello'),
+      //     BottomNavigationBarItem(icon: Icon(Icons.abc), label: 'hello'),
+      //   ],
+      // ),
+    );
+  }
+}
+
+class HadethScreenBottomSheet extends StatelessWidget {
+  const HadethScreenBottomSheet({
+    super.key,
+    required this.screenWidth,
+    required this.screenHeight,
+  });
+
+  final double screenWidth;
+  final double screenHeight;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: screenWidth,
+      height: screenHeight * 0.3,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(25)),
+        boxShadow: [BoxShadow(color: Colors.black26, blurRadius: 10)],
+      ),
+      padding: EdgeInsets.all(20),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            width: screenWidth * 0.15,
+            height: 5,
+            decoration: BoxDecoration(
+              color: Colors.grey[300],
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          SizedBox(height: 20),
+          Text(
+            'تشغيل الصوت',
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Reem kufi',
+            ),
+          ),
+          SizedBox(height: 10),
+          Slider(
+            value: 0.3,
+            onChanged: (value) {},
+            activeColor: AppColors.darkPrimary,
+            inactiveColor: const Color.fromARGB(255, 0, 0, 0),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [Text('0:45'), Text('3:20')],
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                icon: Icon(Icons.skip_previous),
+                iconSize: 40,
+                onPressed: () {},
+              ),
+              SizedBox(width: 15),
+              CircleAvatar(
+                radius: 30,
+                backgroundColor: AppColors.darkPrimary,
+                child: IconButton(
+                  icon: Icon(Icons.play_arrow),
+                  color: Colors.white,
+                  iconSize: 40,
+                  onPressed: () {},
+                ),
+              ),
+              SizedBox(width: 15),
+              IconButton(
+                icon: Icon(Icons.skip_next),
+                iconSize: 40,
+                onPressed: () {},
+              ),
+              SizedBox(width: 15),
+              // زر التكرار
+              IconButton(
+                icon: Icon(Icons.repeat),
+                //color: AppColors.darkPrimary,
+                iconSize: 30,
+                onPressed: () {
+                  // هنا ضع الكود لتفعيل التكرار
+                },
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
