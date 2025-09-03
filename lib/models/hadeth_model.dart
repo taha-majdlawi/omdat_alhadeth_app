ee2c55f164cd4e2012d36d1e1ab49d0456e6e01a
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-09-03 18:45:44 +0300

    create Hadeth class with its attrubites

---
+++ lib/models/hadeth_model.dart
@@ -0,0 +1,13 @@
+class Hadeth {
+  final String hadethText;
+  final int hadethNumber;
+  final String soundUrl;
+  final String explainUrl;
+
+  Hadeth({
+    required this.hadethText,
+    required this.hadethNumber,
+    required this.soundUrl,
+    required this.explainUrl,
+  });
+}
