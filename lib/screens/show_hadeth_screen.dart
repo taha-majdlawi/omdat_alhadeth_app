95ec79109d0e93701ebf591044340ce7abdd4e74
TAHA MAJDLAWI <tahaat659@gmail.com>
2025-09-03 14:23:04 +0300

    ..

--- lib/screens/show_hadeth_screen.dart
+++ lib/screens/show_hadeth_screen.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:omdat_alhadeth/core/constants/app_colors.dart';
+import 'package:omdat_alhadeth/providers/theme_provider.dart';
 
 class ShowHadethScreen extends StatelessWidget {
-  const ShowHadethScreen({super.key});
- 
+  const ShowHadethScreen({super.key, required this.themeProvider});
+  final ThemeProvider themeProvider;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Show Hadeth Screen'),
+        backgroundColor: themeProvider.getIsDarkTheme
+            ? AppColors.darkScaffoldColor
+            : AppColors.lightCardColor,
+        title: Text('الحديث رقم 1', style: TextStyle(fontFamily: 'Amiri')),
+        centerTitle: true,
       ),
-      body: Center(
-        child: Text('This is the Show Hadeth Screen'),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Text('This is the Show Hadeth Screen'),
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/hadeth_item_card_widget.dart
+++ lib/widgets/hadeth_item_card_widget.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:omdat_alhadeth/core/constants/app_colors.dart';
 import 'package:omdat_alhadeth/providers/theme_provider.dart';
+import 'package:omdat_alhadeth/screens/show_hadeth_screen.dart';
 
 class HadethItemCardWidget extends StatelessWidget {
   const HadethItemCardWidget({
@@ -15,27 +16,37 @@ class HadethItemCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      borderOnForeground: true,
-      color: themeProvider.getIsDarkTheme
-          ? AppColors.lightCardColor
-          : AppColors.darkScaffoldColor,
-      child: Padding(
-        padding: const EdgeInsets.only(
-          left: 15.0,
-          right: 15.0,
-          top: 15.0,
-          bottom: 15.0,
-        ),
-        child: Text(
-          textAlign: TextAlign.right,
-          hadethText,
-          maxLines: 1,
-          overflow: TextOverflow.ellipsis,
-          style: TextStyle(
-            fontSize: 22,
-            fontFamily: "Reem Kufi",
-            color: themeProvider.getIsDarkTheme ? Colors.black : Colors.white,
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+  
+          MaterialPageRoute(
+            builder: (context) => ShowHadethScreen(themeProvider: themeProvider,),
+          ),
+        );
+      },
+      child: Card(
+        borderOnForeground: true,
+        color: themeProvider.getIsDarkTheme
+            ? AppColors.lightCardColor
+            : AppColors.darkScaffoldColor,
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 15.0,
+            right: 15.0,
+            top: 15.0,
+            bottom: 15.0,
+          ),
+          child: Text(
+            textAlign: TextAlign.right,
+            hadethText,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              fontSize: 22,
+              fontFamily: "Reem Kufi",
+              color: themeProvider.getIsDarkTheme ? Colors.black : Colors.white,
+            ),
           ),
         ),
       ),
